a7ad2bddbfea97ad84b19d6ccb555e0c8d9906e4
cris <veronikafalcon2004@gmail.com>
2025-10-17 09:45:56 -0500

    Agregando microservicio Especialidad

---
+++ Especialidad/src/main/java/Hospital/Especialidad/Especialidad.java
@@ -0,0 +1,20 @@
+package Hospital.Especialidad;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Especialidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+}
---
+++ Especialidad/src/main/java/Hospital/Especialidad/Repositorio.java
@@ -0,0 +1,7 @@
+package Hospital.Especialidad;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repositorio extends JpaRepository<Especialidad,Long>{
+    
+}
