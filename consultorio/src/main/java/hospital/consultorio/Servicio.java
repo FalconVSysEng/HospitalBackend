1577f9a23d4136eed73b0b21e8a5efbdff18639e
cris <veronikafalcon2004@gmail.com>
2025-10-17 09:50:18 -0500

    Agregando microservicio consultorio

---
+++ consultorio/src/main/java/hospital/consultorio/Servicio.java
@@ -0,0 +1,35 @@
+package hospital.consultorio;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servicio {
+    @Autowired
+    private Repositorio repo;
+    
+    public Consultorio grabar(Consultorio con){
+        return repo.save(con);
+    }
+    
+    public Consultorio buscar(Long id){
+        return repo.findById(id).orElse(null);
+    }
+    
+    public List<Consultorio>listar(){
+        return repo.findAll();
+    }
+    
+    public Consultorio actualizar(Long id,Consultorio con){
+        return repo.findById(id).map(existing->{
+            existing.setNumero(con.getNumero());
+            existing.setPiso(con.getPiso());
+            return repo.save(existing);
+        }).orElse(null);
+    }
+    
+    public void eliminar(Long id){
+        repo.deleteById(id);
+    }
+}
