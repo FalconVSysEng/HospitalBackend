de7888ffea08d23891589194038224cecc04cca0
jhair garcia <garciajhair22@gmail.com>
2025-10-17 00:09:18 -0500

    primer roles

---
+++ roles_empleado/roles_empleado/src/main/java/com/example/roles_empleado/RolesEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.roles_empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesEmpleadoApplication.class, args);
+	}
+
+}
